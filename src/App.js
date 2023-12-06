46bd2497b094c8ff3bab3119369ca4ac28c8d4e1
kbesseghir <k.besseghir@esi-sba.dz>
2023-12-06 14:54:12 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
     <Navbar />
-    
+
+    <div className="row">
+    <div className="col-2 sidebar"><Sidebar /></div>
+    <div className="col-10">test</div>
+    </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+function Sidebar (){
+    return (
+<>
+        
+<ul class="list-group list-group-flush sidebar">
+    <li class="list-group-item"> <a href="#"> Get All Products</a></li>
+    <li class="list-group-item"><a href="#"> Get All Categories</a></li>
+    <li class="list-group-item"><a href="#"> Get All Products</a></li>
+ 
+</ul>
+</>
+
+
+    );
+}
+export default Sidebar;
