f4e464e9545f52c1313a5b690ddb10cdc5e3bf04
kbesseghir <k.besseghir@esi-sba.dz>
2023-12-08 10:33:19 +0100

    commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
+
 function Sidebar (){
     return (
 <>
         
 <ul className="list-group list-group-flush sidebar">
-    <li className="list-group-item"> <a href="#"> Get All Products</a></li>
+    <li className="list-group-item"> <Link to='/products'> Get All Products</Link></li>
     <li className="list-group-item"><a href="#"> Get All Categories</a></li>
     <li className="list-group-item"><a href="#"> Get All Products</a></li>
  
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,39 @@
 function Products(){
+    // const apiUrl=
+    // const getProducts= ()=>{
+
+    // }
     return (
+        <>
         <h1>Product page</h1>
-    );
+        <button className="btn btn-success btn-sm mt-5">Add New Product</button>
+
+        <table class="table table-striped mt-5">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Operations</th>
+        </tr>
+        <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+        <button className="btn btn-danger btn-sm mx-2">Delete</button>
+        <button className="btn btn-info btn-sm mx-2">View</button>
+        <button className="btn btn-primary btn-sm mx-2">Edit</button>
+
+        </td>
+        </tr>
+        </thead>
+        <tbody>
+        
+        </tbody>
+        </table>
+        </>
+        );
+        
 }
 export default Products;
